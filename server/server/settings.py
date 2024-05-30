57d5ae29a6e54dcb27fb2b6bb5c59335fbf52889
Light Lin <lightlin@LightdeMac-Studio.local>
2024-05-30 16:40:42 +0800

    Update: 2024-05-30 16:40:42

--- server/server/settings.py
+++ server/server/settings.py
@@ -201,10 +201,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "static_new",
 ]
 
 MEDIA_URL = '/media/'
